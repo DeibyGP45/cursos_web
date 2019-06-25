9a3d4787f833f9302258d0ef4e018a96d85069ef
DeibyGP <davidgarcip@gmail.com>
2019-06-25 20:04:37 +0200

    Ej 04 25/6

---
+++ JS/04.1_funciones.js
@@ -0,0 +1,35 @@
+/**
+ * @description Calcular la suma de los cuadrados de n numeros
+ * @param {array} aDatos
+ * @returns {number,array}
+ */
+
+function calcularSC(...aDatos) {
+    let r = 0
+    let aCuadrados = []
+    console.log(aDatos)
+
+    for (let i = 0; i < aDatos.length; i++) {
+        const item = aDatos[i];
+        let x = item * item
+        aCuadrados.push(x)
+        r += x
+    }
+    return [r, aCuadrados]
+    return {sumaTotal: r, aCuadrados: aCuadrados}
+
+}
+
+/* let sumaTotal
+let aCuadrados */
+/* let aResultados = calcularSC(1,5,7,8)
+sumaTotal= aResultados[0]
+aCuadrados = aResultados[1] */
+
+//let [sumaTotal, aCuadrados] = calcularSC(1,5,7,8)
+let [aCuadrados,sumaTotal] = calcularSC(1,5,7,8)
+
+
+console.log(calcularSC(2,2,2,4))
+ 
+
---
+++ JS/04_funciones.js
@@ -0,0 +1,22 @@
+
+
+function calcularSC(x, y, ...z) {
+    console.log(arguments)
+    return(x * x) + (y * y)
+    
+}
+calcularSC(2,4) //20
+
+let a = 2
+let b = 4
+calcularSC(a,b) //20
+let x = 2
+let y = 4
+
+
+console.log(calcularSC(y,x))  //20
+
+console.log(calcularSC(4,6,8,4,3))  
+
+
+
