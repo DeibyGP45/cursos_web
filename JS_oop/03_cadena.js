07a39750cee8bdfa71415192db8a5c1019357b74
DeibyGP <davidgarcip@gmail.com>
2019-07-01 18:56:44 +0200

    Cadena

--- JS_oop/03_cadena.js
+++ JS_oop/03_cadena.js
@@ -5,9 +5,19 @@ let version = 'ES6'
 
 let cadena = "Esto no es buena practica"
 let otraCadena = 'Es mejor usar comillas simples'
-let nuevaCadena = `Nuevo en ${version}`
+let nuevaCadena = `Nuevo en ${version}` //Template String
 
 
 let c = console.log(cadena, otraCadena,nuevaCadena)
 
+console.log(cadena.toUpperCase())
+
+
+console.log(cadena.length)
+console.log(cadena[0])
+
+for (let i = 0; i < cadena.length; i++) {
+const item = cadena[i];
+  console.log(item)  
+}
 
