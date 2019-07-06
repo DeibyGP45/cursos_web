9690b47c3d53fdebef81855d09e0cdb03e699cd5
DeibyGP45 <49921920+DeibyGP45@users.noreply.github.com>
2019-07-06 15:00:13 +0200

    6/7

--- JS_oop/17_catalogo.js
+++ JS_oop/17_catalogo.js
@@ -1,1 +1,22 @@
-// Catalos
+// Crear un catalogo con funciones constructivas
+
+/*const art1 = {
+	descripcion: '',
+	codigo:'',
+	precio:'', 
+} */
+
+
+function Articulo(descripcion,codigo,precio) {
+		this.descripcion = descripcion
+		this.codigo = codigo
+		this.precio = precio
+}
+
+const art2 = new Articulo ("Mesa",121,60)
+const art3 = new Articulo ("Silla",60,30)
+
+
+console.log (art2)
+
+
