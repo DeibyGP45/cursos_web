9579e95ea3417554676eba0beb4de4f9a64c2ceb
DeibyGP <davidgarcip@gmail.com>
2019-07-07 22:33:04 +0200

    catalogo 7/7

--- JS_oop/17_catalogo.js
+++ JS_oop/17_catalogo.js
@@ -1,10 +1,10 @@
 // Crear un catalogo con funciones constructivas
 
-/*const art1 = {
-	descripcion: '',
+const art1 = {
+	descripcion: 'Libro',
 	codigo:'',
 	precio:'', 
-} */
+}
 
 
 function Articulo(descripcion,codigo,precio) {
@@ -12,11 +12,12 @@ function Articulo(descripcion,codigo,precio) {
 		this.codigo = codigo
 		this.precio = precio
 }
-
+Articulo.prototype.mIVA = function () {this.precio * 1.21}
 const art2 = new Articulo ("Mesa",121,60)
 const art3 = new Articulo ("Silla",60,30)
 
 
 console.log (art2)
+console.log (art3)
 
 
