872078574c5d6abe25792060e865ff9dfbc00a2c
DeibyGP <davidgarcip@gmail.com>
2019-07-10 13:33:50 +0200

    10/7

--- JS_oop/23_factura_David.js
+++ JS_oop/23_factura_David.js
@@ -1,25 +1,31 @@
 
-const empresa = {
-        Nempresa:'Noseque SA',
-        direccion:'C/ lola 23',
-        teléfono:'915427583',
-        Nif:''
-    }
 
-const infoEmpresa = {
-        Nfactura: 015,
-        IVA: 0.21,
-        fPago: ['Contado','Tarjeta','Online'],
-        fecha:new Date(2019,7,4),
-    }
 
-function Factura(clientes, elementos) {
 
-    
 
+function Factura(clientes) {
+
+    const empresa = {
+            Nempresa:'Noseque SA',
+            direccion:'C/ lola 23',
+            teléfono:'915427583',
+            Nif:'203948543s'
+        }
+
+    const infoEmpresa = {
+            Nfactura: 015,
+            IVA: 0.21,
+            fPago: ['Contado','Tarjeta','Online'],
+            fecha:new Date(2019,7,4),
+        }
+
+    this.empresa = empresa
+    this.infoEmpresa = infoEmpresa
+     
     this.clientes = clientes
-    this.elementos = elementos
+    this.elementos = []
     
+
 }
 
     function Cliente(Ncliente,direccion, telefono,nif) {
@@ -46,12 +52,33 @@ function Elemento (descripcion,precioUnidad,cantidad) {
 //----------------------------------
 //Factura.prototype.
 
+
+
+Factura.prototype.mostrar = function () {
+		let msg = 
+    `
+        ${this.empresa.Nempresa}
+        ${this.empresa.direccion}
+        ${this.empresa.teléfono}
+        ${this.empresa.Nif}
+
+        Cliente: ${this.clientes}
+
+        ${this.infoEmpresa.Nfactura}
+        ${this.infoEmpresa.IVA}
+        ${this.infoEmpresa.fPago[1]}
+        ${this.infoEmpresa.fecha}
+
+
+		Productos : ${this.elementos}
+
+		`
+		console.log(msg)
+	}
+
 Factura.prototype.comprarElemento = function (Elemento) {
     this.elementos.push(Elemento)
 } 
-
-
-
 
 //----------------------------------
 
@@ -59,6 +86,12 @@ const cl01 = new Cliente ('David garcia','C/ mari','698532675','50363458d')
 
 const el01 = new Elemento ('Paquete','45','2')
 
-const factura01 = new Factura ()
 
-console.log(factura01)
+const factura01 = new Factura (cl01)
+
+factura01.comprarElemento(el01)
+
+console.clear()
+
+console.log(factura01.elementos)
+console.log(factura01.mostrar())
