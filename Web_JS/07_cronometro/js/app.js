5c1f6c4c7c57d8cb38f365e874e2cfde448733f2
DeibyGP <davidgarcip@gmail.com>
2019-07-22 21:21:21 +0200

    Cronometro 22/7

---
+++ Web_JS/07_cronometro/js/app.js
@@ -0,0 +1,44 @@
+export class App {
+
+    constructor () {
+
+    console.log('Cargada app') 
+    
+    this.btnPlayPause = document.querySelector('#btn-playPause')
+    //this.btnReset = document.querySelector('#btn-reset')
+    this.output = document.querySelector('output')
+    this.handle = 0
+    this.time = 0
+    
+
+
+    this.btnPlayPause.addEventListener('click',this.play)
+   // this.btnReset.addEventListener('click', this.reset)
+}
+
+    play() {
+
+        this.handle = setInterval( () => {
+            this.time += 1
+            this.render()
+        },1000)
+
+    }
+
+    pause() {
+        clearInternal(this.handle)
+    
+    }
+
+    reset () {
+        this.time = 0
+        this.render()
+
+    }
+
+    render() {
+        this.output.value = this.time
+
+    }
+
+}
